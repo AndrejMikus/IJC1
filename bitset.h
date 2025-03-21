c87762e887fd41b5e441478fad7541a67f12637a
Andrej Mikus <andrej.mikus01@gmail.com>
2025-03-21 11:10:09 +0100

    feat: typedefs and bitset_create macro definition

--- bitset.h
+++ bitset.h
@@ -4,3 +4,10 @@
 // Prelozeno: gcc
 //
 
+#include <limits.h>
+
+typedef unsigned long * bitset_t; // type of bit array
+
+typedef unsigned long bitset_index_t;
+
+#define bitset_create(jmeno_pole, velikost) (unsigned long jmeno_pole[(velikost) / CHAR_BIT + 1]) = { (velikost) }
